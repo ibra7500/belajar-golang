3e74edcc6742834c04f736327a43b570994ca651
Ibrahim <rahadian.ibrahim@gmail.com>
2025-09-16 20:30:05 +0700

    tambah if statement

--- comparison/main.go
+++ comparison/main.go
@@ -13,10 +13,25 @@ func main() {
 	fmt.Scanln(&angka2)
 
 	// Perbandingan
-	fmt.Printf("%d < %d ? %v \n", angka1, angka2, angka1 < angka2)
-	fmt.Printf("%d > %d ? %v \n", angka1, angka2, angka1 > angka2)
-	fmt.Printf("%d == %d ? %v \n", angka1, angka2, angka1 == angka2)
-	fmt.Printf("%d != %d ? %v \n", angka1, angka2, angka1 != angka2)
-	fmt.Printf("%d <= %d ? %v \n", angka1, angka2, angka1 <= angka2)
-	fmt.Printf("%d >= %d ? %v \n", angka1, angka2, angka1 >= angka2)
+	// fmt.Printf("%d < %d ? %v \n", angka1, angka2, angka1 < angka2)
+	// fmt.Printf("%d > %d ? %v \n", angka1, angka2, angka1 > angka2)
+	// fmt.Printf("%d == %d ? %v \n", angka1, angka2, angka1 == angka2)
+	// fmt.Printf("%d != %d ? %v \n", angka1, angka2, angka1 != angka2)
+	// fmt.Printf("%d <= %d ? %v \n", angka1, angka2, angka1 <= angka2)
+	// fmt.Printf("%d >= %d ? %v \n", angka1, angka2, angka1 >= angka2)
+
+	// Kalo pake if statement
+	if angka1 < angka2 {
+		fmt.Println("Angka", angka1 , "lebih kecil daripada angka", angka2)
+	} else if angka1 > angka2 {
+		fmt.Println("Angka", angka1 , "lebih besar daripada angka", angka2)
+	} else if angka1 == angka2 {
+		fmt.Println("Angka", angka1 , "sama dengan daripada angka", angka2)
+	}
+
+	// shorthand if
+	if angka3 := angka1*2; angka3 < 10 {
+		fmt.Println("Hasil kurang dari 10: ", angka3)
+	}
+	
 }
